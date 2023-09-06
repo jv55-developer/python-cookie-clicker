0b665c3e6ee3bd47b8e10467b3caf9371052b9a5
Jason Vandeyar <jasonvandeyar55@gmail.com>
2023-09-06 05:02:34 +0200

    cookie clicking running, and interval set for 5 seconds... still busy

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+import time
 
 # Keep Chrome open
 chrome_options = webdriver.ChromeOptions()
@@ -9,3 +10,18 @@ driver = webdriver.Chrome(options=chrome_options)
 
 # Open site
 driver.get("https://orteil.dashnet.org/experiments/cookie/")
+
+cookie = driver.find_element(By.CSS_SELECTOR, value="#cookie")
+
+store = driver.find_elements(By.CSS_SELECTOR, value="#store div")
+print(type(store))
+
+# click_cookie = True
+# start_time = int(time.time())
+# while click_cookie:
+#
+#     cookie.click()
+#
+#     if int(time.time()) == start_time + 5:
+#
+#         start_time = int(time.time())
