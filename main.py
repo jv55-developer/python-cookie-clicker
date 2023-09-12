3ea5469d3134433866a88a47fde380e232b584fc
Jason Vandeyar <jasonvandeyar55@gmail.com>
2023-09-12 04:37:56 +0200

    able to loop through list of uprades and click on them if I have enough money. Getting a stale element error

--- main.py
+++ main.py
@@ -11,17 +11,24 @@ driver = webdriver.Chrome(options=chrome_options)
 # Open site
 driver.get("https://orteil.dashnet.org/experiments/cookie/")
 
+store = driver.find_elements(By.CSS_SELECTOR, value="#store div")
 cookie = driver.find_element(By.CSS_SELECTOR, value="#cookie")
 
-store = driver.find_elements(By.CSS_SELECTOR, value="#store div")
-print(type(store))
+click_cookie = True
+start_time = int(time.time())
+while click_cookie:
+    money = int(driver.find_element(By.ID, value="money").text)
+    cookie.click()
 
-# click_cookie = True
-# start_time = int(time.time())
-# while click_cookie:
-#
-#     cookie.click()
-#
-#     if int(time.time()) == start_time + 5:
-#
-#         start_time = int(time.time())
+    if int(time.time()) == start_time + 5:
+
+        for store_item in store[:-1]:
+            item = store_item.find_element(By.TAG_NAME, value="b").text
+            item_value = int((item.split("- ")[1]).replace(",", ""))
+            print(item_value)
+
+            if money > item_value:
+                store_item.click()
+
+        start_time = int(time.time())
+
