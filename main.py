fe460b884ed8971df659df966ee024b84ebe88e8
Jason Vandeyar <jasonvandeyar55@gmail.com>
2023-09-13 04:49:10 +0200

    this isn't working. Stale element error

--- main.py
+++ main.py
@@ -11,24 +11,28 @@ driver = webdriver.Chrome(options=chrome_options)
 # Open site
 driver.get("https://orteil.dashnet.org/experiments/cookie/")
 
-store = driver.find_elements(By.CSS_SELECTOR, value="#store div")
+
 cookie = driver.find_element(By.CSS_SELECTOR, value="#cookie")
+store = driver.find_elements(By.CSS_SELECTOR, value="#store div")
+list_of_store_items = store
 
 click_cookie = True
 start_time = int(time.time())
 while click_cookie:
-    money = int(driver.find_element(By.ID, value="money").text)
+    money = int(driver.find_element(By.ID, value="money").text.replace(",", ""))
     cookie.click()
 
     if int(time.time()) == start_time + 5:
 
-        for store_item in store[:-1]:
-            item = store_item.find_element(By.TAG_NAME, value="b").text
-            item_value = int((item.split("- ")[1]).replace(",", ""))
-            print(item_value)
+        for store_item in list_of_store_items[:-1]:
+            item = store_item.text
+            item_value = int(item.split("\n")[0].split("- ")[1].replace(",", ""))
 
             if money > item_value:
-                store_item.click()
+                item_id = store_item.get_attribute("id")
+                item_to_click = driver.find_element(By.ID, value=item_id)
+                item_to_click.click()
+                list_of_store_items.remove(store_item)
 
         start_time = int(time.time())
 
